5a5e70d2f331c3060ade8f49e5196a4bf5529ded
itlevelsfyi <49151978+itlevelsfyi@users.noreply.github.com>
2019-04-20 15:50:14 +0800

    Create insertion_sort.js

---
+++ src/chapter_1/insertion_sort.js
@@ -0,0 +1,12 @@
+const insertion_sort = A => {
+  for (let j = 1; j < A.length; j++) {
+    const key = A[j]
+    let i = j - 1
+    while (i >= 0 && A[i] > key) {
+      A[i+1] = A[i]
+      i--
+    }
+    A[i+1] = key
+  }
+}
+export default insertion_sort
