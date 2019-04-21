a62751703aaeff9a378e554151c7ada3a46280e6
itlevelsfyi <49151978+itlevelsfyi@users.noreply.github.com>
2019-04-21 21:17:03 +0800

    Create add_binary_integer.js

---
+++ src/chapter_1/add_binary_integer.js
@@ -0,0 +1,33 @@
+const add_binary_integer = (A, B) => {
+  if (!Array.isArray(A) || A.length <= 0) {
+    return B.unshift(0)
+  } else if (!Array.isArray(B) || B.length <= 0) {
+    return A.unshift(0)
+  } else {
+    let carry = 0
+    let result = []
+    const reverse_index = (Arr, idx) => Arr.length - idx - 1
+    let i = 0
+    while (i < A.length || i < B.length) {
+      const ai = (reverse_index(A,i) >= 0) ? A[reverse_index(A,i)] : 0
+      const bi = (reverse_index(B,i) >= 0) ? B[reverse_index(B,i)] : 0
+      const ci = ai + bi + carry
+      if (ci === 3) {
+        carry = 1
+        result.unshift(1)
+      } else if (ci === 2) {
+        carry = 1
+        result.unshift(0)
+      } else if (ci === 1) {
+        result.unshift(1)
+      } else {
+        result.unshift(0)
+      }
+      i++
+    }
+    result.unshift(carry)
+    return result
+  }
+}
+
+module.exports = add_binary_integer
