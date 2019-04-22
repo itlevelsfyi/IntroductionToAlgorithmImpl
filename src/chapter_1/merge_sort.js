c567b089633be17cfabf6c39f08da68c834bf6bb
itlevelsfyi <49151978+itlevelsfyi@users.noreply.github.com>
2019-04-22 12:24:20 +0800

    Update merge_sort.js

--- src/chapter_1/merge_sort.js
+++ src/chapter_1/merge_sort.js
@@ -1,5 +1,5 @@
 const merge = (A, p, q, r) => {
-  const n1 = q - p
+  const n1 = q - p + 1
   const n2 = r - q
   let L = []
   let R = []
@@ -7,7 +7,7 @@ const merge = (A, p, q, r) => {
     L.push(A[p+i])
   }
   for (let j = 0; j < n2; j++) {
-    R.push(A[q+j])
+    R.push(A[q+1+j])
   }
   // add sentinal
   L.push(Infinity)
@@ -15,7 +15,7 @@ const merge = (A, p, q, r) => {
   
   let i = 0
   let j = 0
-  for (let k = p; k < r; k++) {
+  for (let k = p; k <= r; k++) {
     if (L[i] <= R[j]) {
       A[k] = L[i]
       i++
@@ -30,13 +30,13 @@ const merge_sort_impl = (A, p, r) => {
   if (p < r) {
     q = Math.floor((p + r) / 2)
     merge_sort_impl(A, p, q)
-    merge_sort_impl(A, q, r)
+    merge_sort_impl(A, q + 1, r)
     merge(A, p, q, r)
   }
 }
 
 const merge_sort = A => {
-  merge_sort_impl(A, 0, A.length)
+  merge_sort_impl(A, 0, A.length - 1)
 }
 
 module.exports = merge_sort
