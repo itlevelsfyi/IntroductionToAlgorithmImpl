5eaa2186bb3301d4c7aee7f9b059b0bc3c549ef3
itlevelsfyi <49151978+itlevelsfyi@users.noreply.github.com>
2019-04-23 17:00:22 +0800

    Update selection_sort_test.js

--- test/chapter_1/selection_sort_test.js
+++ test/chapter_1/selection_sort_test.js
@@ -1,14 +1,20 @@
 const assert = require('assert')
 const selection_sort = require('../../src/chapter_1/selection_sort')
+const sort_data = require('../sort_data/sort_data')
+const num_ascending = require('../sort_data/num_ascending')
 
 describe('Selection Sort Test', () => {
-  let data = [31, 41, 59, 26, 41, 58]
   it('Basic Test', done => {
-    let arr = [...data]
-    selection_sort(arr)
-    for (let i = 1; i < arr.length; i++) {
-      assert(arr[i] >= arr[i-1])
-    }
+    sort_data.forEach(data => {
+      let arr = [...data]
+      let sorted = [...data].sort(num_ascending)
+      selection_sort(arr)
+      for (let i = 1; i < arr.length; i++) {
+        assert(arr[i] >= arr[i-1])
+        assert(sorted[i] === arr[i])
+      }
+    })
+    
     done()
   })
   it('Test empty case', done => {
