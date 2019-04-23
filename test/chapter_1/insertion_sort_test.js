0116045a4033f5f7dff2d4bc63db634e1012f87b
itlevelsfyi <49151978+itlevelsfyi@users.noreply.github.com>
2019-04-23 16:44:47 +0800

    Update insertion_sort_test.js

--- test/chapter_1/insertion_sort_test.js
+++ test/chapter_1/insertion_sort_test.js
@@ -1,23 +1,31 @@
 const assert = require('assert')
 const insertion_sort = require('../../src/chapter_1/insertion_sort')
 const insertion_sort_descending = require('../../src/chapter_1/insertion_sort_descending')
+const sort_data = require('../sort_data/sort_data')
 
 describe('Basic Sort Test', () => {
-  let data = [31, 41, 59, 26, 41, 58]
   it('Exercise-2.1.1', done => {
-    let arr = [...data]
-    insertion_sort(arr)
-    for (let i = 1; i < arr.length; i++) {
-      assert(arr[i] >= arr[i-1])
-    }
+    sort_data.forEach(data => {
+      let arr = [...data]
+      let sorted = [...data].sort()
+      insertion_sort(arr)
+      for (let i = 1; i < arr.length; i++) {
+        assert(arr[i] >= arr[i-1])
+        assert(arr[i] === sorted[i])
+      }
+    })
     done()
   })
   it('Exercise-2.1.2', done => {
-    let arr = [...data]
-    insertion_sort_descending(arr)
-    for (let i = 1; i < arr.length; i++) {
-      assert(arr[i] <= arr[i-1])
-    }
+    sort_data.forEach(data => {
+      let arr = [...data]
+      let sorted = [...data].sort()
+      insertion_sort_descending(arr)
+      for (let i = 1; i < arr.length; i++) {
+        assert(arr[i] <= arr[i-1])
+        assert(arr[i] === sorted[i])
+      }
+    })
     done()
   })
 })
