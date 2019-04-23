e4a5d812d4cb0bf1a74c1da6ed260b3997787b20
itlevelsfyi <49151978+itlevelsfyi@users.noreply.github.com>
2019-04-23 16:58:13 +0800

    Update merge_sort_test.js

--- test/chapter_1/merge_sort_test.js
+++ test/chapter_1/merge_sort_test.js
@@ -1,14 +1,20 @@
 const assert = require('assert')
 const merge_sort = require('../../src/chapter_1/merge_sort')
+const sort_data = require('../sort_data/sort_data')
+const num_ascending = require('../sort_data/num_ascending')
 
 describe('Merge sort test', () => {
   let data = [31, 41, 59, 26, 41, 58]
   it('Basic sort test', done => {
-    let arr = [...data]
-    merge_sort(arr)
-    for (let i = 1; i < arr.length; i++) {
-      assert(arr[i] >= arr[i-1])
-    }
+    sort_data.forEach(data => {
+      let arr = [...data]
+      let sorted = [...data].sort(num_ascending)
+      merge_sort(arr)
+      for (let i = 1; i < arr.length; i++) {
+        assert(arr[i] >= arr[i-1])
+        assert(arr[i] === sorted[i])
+      }
+    })
     done()
   })
   it('One element test', done => {
