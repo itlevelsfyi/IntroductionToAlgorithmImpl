26c86e569b36e912835333c3b540f20f123242fa
itlevelsfyi <49151978+itlevelsfyi@users.noreply.github.com>
2019-04-23 16:51:22 +0800

    Update insertion_sort_test.js

--- test/chapter_1/insertion_sort_test.js
+++ test/chapter_1/insertion_sort_test.js
@@ -2,12 +2,13 @@ const assert = require('assert')
 const insertion_sort = require('../../src/chapter_1/insertion_sort')
 const insertion_sort_descending = require('../../src/chapter_1/insertion_sort_descending')
 const sort_data = require('../sort_data/sort_data')
+const num_ascending = require('../sort_data/num_ascending')
 
 describe('Basic Sort Test', () => {
   it('Exercise-2.1.1', done => {
     sort_data.forEach(data => {
       let arr = [...data]
-      let sorted = [...data].sort()
+      let sorted = [...data].sort(num_ascending)
       insertion_sort(arr)
       console.log(`arr = ${JSON.stringify(arr)}`)
       console.log(`sorted = ${JSON.stringify(sorted)}`)
@@ -21,7 +22,7 @@ describe('Basic Sort Test', () => {
   it('Exercise-2.1.2', done => {
     sort_data.forEach(data => {
       let arr = [...data]
-      let sorted = [...data].sort()
+      let sorted = [...data].sort(num_ascending)
       insertion_sort_descending(arr)
       for (let i = 1; i < arr.length; i++) {
         assert(arr[i] <= arr[i-1])
