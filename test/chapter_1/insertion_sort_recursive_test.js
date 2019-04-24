8916e7451c2b9a0a8938f3f948e30d54fe7cc8b4
itlevelsfyi <49151978+itlevelsfyi@users.noreply.github.com>
2019-04-24 10:04:45 +0800

    Create insertion_sort_recursive_test.js

---
+++ test/chapter_1/insertion_sort_recursive_test.js
@@ -0,0 +1,19 @@
+const assert = require('assert')
+const insertion_sort = require('../../src/chapter_1/insertion_sort_recursive')
+const sort_data = require('../sort_data/sort_data')
+const num_ascending = require('../sort_data/num_ascending')
+
+describe('Basic Sort Test', () => {
+  it('Basic case', done => {
+    sort_data.forEach(data => {
+      let arr = [...data]
+      let sorted = [...data].sort(num_ascending)
+      insertion_sort(arr)
+      for (let i = 1; i < arr.length; i++) {
+        assert(arr[i] >= arr[i-1])
+        assert(arr[i] === sorted[i])
+      }
+    })
+    done()
+  })
+})
