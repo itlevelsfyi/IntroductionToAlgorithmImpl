1d2770f3e33310a4f38a0fbc401b11610ea7b0e1
itlevelsfyi <49151978+itlevelsfyi@users.noreply.github.com>
2019-04-25 17:21:29 +0800

    Create insertion_sort_with_binary_search.js

---
+++ src/chapter_1/insertion_sort_with_binary_search.js
@@ -0,0 +1,40 @@
+insertion_sort = A => {
+  const binary_search_insert_point = (A, left, right, elem) => {
+    if (elem < A[left]) {
+      return left
+    } else if (elem > A[right]) {
+      return right + 1
+    }
+    let pos = Math.floor((left + right) / 2)
+    if (left <= right) {
+      let l = left
+      let r = right
+      while (l <= r) {
+        if (elem >= A[r]) {
+          return r + 1
+        } else if (elem <= A[l]) {
+          return l
+        }
+        pos = Math.floor((l+r) / 2)
+        if (elem === A[pos]) {
+          return pos
+        } else if (elem < A[pos]) {
+          r = pos - 1
+        } else {
+          l = pos + 1
+        }
+      }
+    }
+    return pos
+  }
+  for (let i = 1; i < A.length; i++) {
+    // [0..i-1] are sorted
+    const elem = A[i]
+    const idx = binary_search_insert_point(A, 0, i-1, elem)
+    for (p = i; p > idx;  p--) {
+      A[p] = A[p-1]
+    }
+    A[idx] = elem
+  }
+}
+module.exports = insertion_sort
