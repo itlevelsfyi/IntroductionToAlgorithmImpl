5ae80ac5aa93c7b359b301864e725af3dbcfd221
itlevelsfyi <49151978+itlevelsfyi@users.noreply.github.com>
2019-04-29 21:53:28 +0800

    Create horner_rule_naive.js

---
+++ src/chapter_1/horner_rule_naive.js
@@ -0,0 +1,18 @@
+const pow = (x, n) => {
+  let y = 1
+  while (n > 0) {
+    y *= x
+    n--
+  }
+  return y
+}
+
+horner_rule_naive = (A, x) => {
+  let y = 0
+  for (let i = 0; i < A.length; i++) {
+    y += (A[i] * pow(x,i)
+  }
+  return y
+}
+
+module.exports = horner_rule_naive
