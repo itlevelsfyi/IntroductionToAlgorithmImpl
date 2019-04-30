eeb8ea987b49eb89fbf83a9d6677cc20d9575276
itlevelsfyi <49151978+itlevelsfyi@users.noreply.github.com>
2019-04-30 13:58:04 +0800

    Create determine_inversions_test.js

---
+++ test/chapter_1/determine_inversions_test.js
@@ -0,0 +1,16 @@
+const assert = require('assert')
+const determine_inversions = require('../../src/chapter_1/determine_inversions')
+
+describe('Determine Inversions Tests', () => {
+  
+  it('Basic determine inversion test', done => {
+    let A = [2,3,8,6,1]
+    assert(determine_inversions(A), 5)
+    done()
+  })
+  it('No inversions for already sorted array', done => {
+    let A = [1,2,3,5,8,10]
+    assert(determine_inversions(A), 0)
+    done()
+  })
+})
