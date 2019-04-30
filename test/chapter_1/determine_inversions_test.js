387cf510b5cbed5726a13518dc45c1360080808b
itlevelsfyi <49151978+itlevelsfyi@users.noreply.github.com>
2019-04-30 14:02:42 +0800

    Update determine_inversions_test.js

--- test/chapter_1/determine_inversions_test.js
+++ test/chapter_1/determine_inversions_test.js
@@ -10,6 +10,7 @@ describe('Determine Inversions Tests', () => {
   })
   it('No inversions for already sorted array', done => {
     let A = [1,2,3,5,8,10]
+    console.log(determine_inversions(A))
     assert(determine_inversions(A), 0)
     done()
   })
