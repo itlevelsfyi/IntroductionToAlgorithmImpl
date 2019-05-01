6bc14559e0527c52cd59d53d3ce4d1d274e6fa78
itlevelsfyi <49151978+itlevelsfyi@users.noreply.github.com>
2019-05-01 11:45:45 +0800

    Update minimum_subarray_test.js

--- test/chapter_4/minimum_subarray_test.js
+++ test/chapter_4/minimum_subarray_test.js
@@ -4,10 +4,11 @@ const find_maximum_subarray = require('../../src/chapter_4/find_maximum_subarray
 describe('Find maximum subarray test', () => {
   it('find maximum sub array test', done => {
     let data = [13,-3,-25,20,-3,-16,-23,18,20,-7,12,-5,-22,15,-4,7]
-    let [min_left, min_right, min] = find_maximum_subarray(data)
-    assert(min_left == 7)
-    assert(min_right ==10)
-    assert(min == 43)
+    let [max_left, max_right, max] = find_maximum_subarray(data)
+    console.log(`max_left=${max_left}, max_right=${max_right}, max =${max}`)
+    assert(max_left == 7)
+    assert(max_right == 10)
+    assert(max == 43)
     done()
   })
 })
